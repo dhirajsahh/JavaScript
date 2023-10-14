9e0026d60bbc316f4d519bf4bd3b0e44e995b850
dhirajsahh <dhirajmahato2019@gmail.com>
2023-10-14 10:12:51 +0545

    type conversion

--- datatype.js
+++ datatype.js
@@ -2,4 +2,4 @@ const accountId = 12345;
 let email = "dhirajmahato2019@gmail.com";
 accountcity = "siraha";
 let accountname;
-console.table([accountId, email, accountcity, accountname]);
+// console.table([accountId, email, accountcity, accountname]);
---
+++ typeconversion.js
@@ -0,0 +1,34 @@
+let fname = "Dhiraj";
+console.log(typeof fname);
+
+let fnamenumber = Number(fname);
+console.log(fnamenumber);
+console.log(typeof fnamenumber);
+
+let mark = "33";
+let markNumber = Number(mark);
+console.log(markNumber);
+console.log(typeof markNumber);
+let adress = "siraha";
+adressnumber = Number(adress);
+console.log(typeof adressnumber);
+console.log(adressnumber);
+// "33"=>33
+// "33abc"=>NaN
+// typeof NaN=>number
+// "ram"=>NaN
+//true=>1;
+//false=>0;
+//Capital letter infront of Datatypes like number=>Number, string=>String,boolean=>Boolean is used for data conversion.
+
+let isLoggedIn = true;
+const number = Number(isLoggedIn);
+console.log(number);
+console.log(typeof number);
+// 1=>true; 0=>false;
+//""=>false empty string =>false
+// "ram"=>true;
+let someNumber = 33;
+let stringNumber = String(someNumber); //33=>"33"
+console.log(stringNumber);
+console.log(typeof stringNumber);
