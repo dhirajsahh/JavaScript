aeb1213d0212aaff96781f3d207c1f06c2c47b92
dhirajsahh <dhirajmahato2019@gmail.com>
2023-10-15 20:34:46 +0545

    Array

---
+++ Array.js
@@ -0,0 +1,10 @@
+const arr = [0, 1, 2, 3, 4, 5];
+const arr1 = arr.slice(1, 3);
+const arr2 = arr.splice(1, 3);
+console.log(arr);
+console.log(arr1);
+console.log(arr2);
+console.log(arr.includes(2));
+
+//splice ma original array manipulate hunxa
+//slice ma original array manipulate hudaina
