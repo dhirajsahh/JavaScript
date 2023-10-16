05223a39fce5f7c7bae6c9b126b74ca13043cefc
dhirajsahh <dhirajmahato2019@gmail.com>
2023-10-16 15:11:06 +0545

    IIFE

---
+++ iife.js
@@ -0,0 +1,12 @@
+(function sum() {
+  console.log(3 + 4);
+})();
+(() => {
+  console.log("Hello world");
+})();
+const obj = function createobj() {
+  return { name: "Dhiraj", adress: "siraha" };
+};
+console.log(obj());
+const arrowobj = () => ({ name: "ram" });
+console.log(arrowobj());
